c01c752107dc63ead7142a716001be8f9e6d47a0
Alba Cuatecontzi <isa081398@gmail.com>
2020-06-03 22:52:24 -0500

    editar manage.py

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto3',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
