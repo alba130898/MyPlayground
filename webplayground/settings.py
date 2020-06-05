376a918018416ea6bc366383d10cfabd926f660e
Alba Cuatecontzi <isa081398@gmail.com>
2020-06-05 00:01:19 -0500

    editar manage.py

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth redirects
+LOGIN_REDIRECT_URL = 'pages:pages'
